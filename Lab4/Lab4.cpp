50eb01ec15f7f6d549622195d2e9bb524a9474d7
AndriiSmolinskyi <awful.braid@gmail.com>
2022-12-21 02:08:43 +0200

    Labor4
    Labor4

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    
+    srand(time(NULL));
+    int const SIZE = 10;
+
+    int arr[SIZE];
+
+    for ( int i = 0; i < SIZE; i++)
+    {
+        arr[i] = rand();
+    }
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+}
+
